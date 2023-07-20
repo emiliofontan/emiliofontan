75a0e957d3d5d2afe44dc19b8a061c164d9e8e8a
emiliofontan <137268816+emiliofontan@users.noreply.github.com>
2023-07-20 11:21:07 +0200

    Update C_2_arrais_promedios de cada un y totales.c

--- C_2_arrais_promedios de cada un y totales.c
+++ C_2_arrais_promedios de cada un y totales.c
@@ -1,20 +1,17 @@
-/* declaramos 3 arrays :
+/* 
+1º Declaramos 3 arrays :
+-notas tamara
+- notas marcos
+- notas diego
 
-notas tamara, notas marcos y notas diego
-
-cada array tendra 5 notas 
-
-1. pedimoslas por pantalla y las mostramos 
-2. mostrar promedio de cada alumno
-3. mostrar promedio de todos os alumnos
-
-el promedio es "suma"
-
+2º Cada array tendra 5 notas,
+3º Pedimoslas por pantalla y las mostramos, 
+4º Mostrar promedio de cada alumno,
+5º Mostrar promedio de todos os alumnos
 */
 
 #include <stdio.h> 
 int main() 
-
 { 
     
 int i; 
@@ -24,38 +21,32 @@ float notasdiego[5];
 float sumat, sumam, sumad, promediot, promediom, promediod, sumatotal, promediototal;
 
 for (i=0; i<5; i++)
-{printf( "\ndime 5 notas de tamara  %i  ",i);
-scanf("%f",&notastamara[i]);
-sumat=sumat+notastamara[i];       
-}
+    {printf( "\ndime 5 notas de tamara  %i  ",i);
+    scanf("%f",&notastamara[i]);
+    sumat=sumat+notastamara[i];}
 
 for (i=0; i<5; i++)
-{printf( "\ndime 5 notas de marcos %i  ",i);
-scanf("%f",&notasmarcos[i]);
-sumam=sumam+notasmarcos[i];   
-}
+    {printf( "\ndime 5 notas de marcos %i  ",i);
+    scanf("%f",&notasmarcos[i]);
+    sumam=sumam+notasmarcos[i];}
 
 for (i=0; i<5; i++)
-{printf( "\ndime 5 notas de diego  %i  ",i);
-scanf("%f",&notasdiego[i]);
-sumad=sumad+notasdiego[i]; 
-}
+    {printf( "\ndime 5 notas de diego  %i  ",i);
+    scanf("%f",&notasdiego[i]);
+    sumad=sumad+notasdiego[i];}
 
 promediot=(sumat/5);
-printf("\nPromedio de tamara %2.f ",promediot);
+    printf("\nPromedio de tamara %2.f ",promediot);
 promediom=(sumam/5);
-printf("\nPromedio de marcos %2.f ",promediom);
+    printf("\nPromedio de marcos %2.f ",promediom);
 promediod=(sumad/5);
-printf("\nPromedio de diego %2.f ",promediod);
+    printf("\nPromedio de diego %2.f ",promediod);
 
-for (i=0; i<5; i++)
-{
-sumatotal=sumatotal+notastamara[i]+notasdiego[i]+notasmarcos[i];
-}
+for (i=0; i<5; i++){
+    sumatotal=sumatotal+notastamara[i]+notasdiego[i]+notasmarcos[i];}
 
 promediototal=(sumatotal/15);
-printf("\nPromedio de todos %2.f ",promediototal);
-    
+    printf("\nPromedio de todos %2.f ",promediototal);
 
 }
 
